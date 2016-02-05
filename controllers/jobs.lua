8a38fed59487d1492a8912c6f3003ddbbadc10d6
Etiene Dalcol <dalcol@etiene.net>
2016-02-05 14:31:15 +0100

    Add admin page for approving jobs

--- controllers/jobs.lua
+++ controllers/jobs.lua
@@ -2,7 +2,7 @@ local M = {}
 local Job = require "sailor.model"("job")
 
 function M.index(page)
-	local jobs = Job:find_all()
+	local jobs = Job:find_all('approved = 1')
 	page:render('index',{jobs = jobs})
 end
 
@@ -13,9 +13,9 @@ function M.add(page)
 	if next(page.POST) then
 		job:get_post(page.POST)
 		job.posted_date = date():fmt("%Y-%m-%d %T")
-		job.allow_remote = job.allow_remote == 'on' and true
-		job.visa_sponsoring  = job.visa_sponsoring == 'on' and true
-		job.relocation_aid = job.relocation_aid == 'on' and true
+		job.allow_remote = job.allow_remote == 'on' and true or false
+		job.visa_sponsoring  = job.visa_sponsoring == 'on' and true or false
+		job.relocation_aid = job.relocation_aid == 'on' and true or false
 		job.approved = false
 		saved = job:save()
 		if saved then
@@ -61,7 +61,21 @@ function M.delete(page)
 end
 
 function M.admin(page)
+	local pw = (require "conf.conf").admin_pass
+	if not next(page.GET) or page.GET.pw ~= pw then return 404 end
 
+	if next(page.POST) and page.POST.approve_job then
+		local job_to_approve = Job:find_by_id(page.POST.approve_job)
+		job_to_approve.allow_remote = job_to_approve.allow_remote and true or false
+		job_to_approve.visa_sponsoring  = job_to_approve.visa_sponsoring and true or false
+		job_to_approve.relocation_aid = job_to_approve.relocation_aid and true or false
+		job_to_approve.approved = true
+		job_to_approve:save()
+	end
+
+	local admin = true
+	local jobs = Job:find_all("approved is NULL")
+	page:render('index',{jobs = jobs, admin=admin})
 end
 
 return M
